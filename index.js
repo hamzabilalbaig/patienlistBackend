c275fc62e2d647b6a84e02dffc821f60f10bb339
hamza <hamzabilalbaig@gmail.com>
2023-06-01 03:14:43 +0500

    cors

--- index.js
+++ index.js
@@ -11,32 +11,7 @@ app.use(bodyParser.json());
 //     origin: "https://elec-patient-records.vercel.app/",
 //   })
 // );
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://elec-patient-records.vercel.app/"
-  );
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
+app.use(cors());
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(process.env.PORT, () =>
