875cf92e03e5fb4e60e0249ffb0f668738875bff
hamza <hamzabilalbaig@gmail.com>
2023-06-01 03:17:17 +0500

    cors

--- index.js
+++ index.js
@@ -3,23 +3,25 @@ const app = express();
 require("dotenv").config();
 const cors = require("cors");
 
-//middle wares
+// Middlewares
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-// app.use(
-//   cors({
-//     origin: "https://elec-patient-records.vercel.app/",
-//   })
-// );
+
+// Enable CORS for all routes
 app.use(cors());
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`)
-);
 // Add headers before the routes are defined
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://elec-patient-records.vercel.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
-//routes
+// Routes
 const patientRoutes = require("./routes/patient.routes");
 const userRoutes = require("./routes/user.routes");
 const notesRoutes = require("./routes/notes.routes");
@@ -29,3 +31,8 @@ app.use("/api/patient", patientRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/notes", notesRoutes);
 app.use("/api/templates", templatesRoutes);
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`)
+);
