699bd51d5f80a66b85c9f502184ef582277eb6e1
hamza <hamzabilalbaig@gmail.com>
2023-06-01 03:12:32 +0500

    cors

--- index.js
+++ index.js
@@ -6,17 +6,11 @@ const cors = require("cors");
 //middle wares
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: "https://elec-patient-records.vercel.app/",
-  })
-);
-
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`)
-);
-// Add headers before the routes are defined
+// app.use(
+//   cors({
+//     origin: "https://elec-patient-records.vercel.app/",
+//   })
+// );
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader(
@@ -43,6 +37,12 @@ app.use(function (req, res, next) {
   // Pass to next layer of middleware
   next();
 });
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`)
+);
+// Add headers before the routes are defined
 
 //routes
 const patientRoutes = require("./routes/patient.routes");
