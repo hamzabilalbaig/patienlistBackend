6d24f4819cc0d1290ac7e4f0b15ad1617e8384d0
hamza <hamzabilalbaig@gmail.com>
2023-06-07 15:39:32 +0500

    d

--- index.js
+++ index.js
@@ -17,23 +17,23 @@ app.get("/api/home", (req, res) => {
   res.json({ message: "hello" });
 });
 
-// const patientRoutes = require("./routes/patient.routes");
-// const userRoutes = require("./routes/user.routes");
-// const notesRoutes = require("./routes/notes.routes");
-// const templatesRoutes = require("./routes/templates.routes");
-// const vitalRouter = require("./routes/vitals.routes");
-// const resultRouter = require("./routes/results.routes");
-// const billsRouter = require("./routes/bills.routes");
-// const billsItemsRouter = require("./routes/billsItems.routes");
+const patientRoutes = require("./routes/patient.routes");
+const userRoutes = require("./routes/user.routes");
+const notesRoutes = require("./routes/notes.routes");
+const templatesRoutes = require("./routes/templates.routes");
+const vitalRouter = require("./routes/vitals.routes");
+const resultRouter = require("./routes/results.routes");
+const billsRouter = require("./routes/bills.routes");
+const billsItemsRouter = require("./routes/billsItems.routes");
 
-// app.use("/api/patient", patientRoutes);
-// app.use("/api/user", userRoutes);
-// app.use("/api/notes", notesRoutes);
-// app.use("/api/templates", templatesRoutes);
-// app.use("/api/vitals", vitalRouter);
-// app.use("/api/result", resultRouter);
-// app.use("/api/bills", billsRouter);
-// app.use("/api/billsItems", billsItemsRouter);
+app.use("/api/patient", patientRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/notes", notesRoutes);
+app.use("/api/templates", templatesRoutes);
+app.use("/api/vitals", vitalRouter);
+app.use("/api/result", resultRouter);
+app.use("/api/bills", billsRouter);
+app.use("/api/billsItems", billsItemsRouter);
 
 app.listen(port, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`)
